bfe9547bd7fd3591c6069fbb6c92d4d1bc55c9b2
NOVA <nova@Mac-mini-de-NOVA.home>
2021-11-03 14:20:07 +0100

    Add navigation

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,22 @@
 package com.example;
 
+import com.example.game.Game;
+
 /**
  * Hello world!
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world!");
+        // Crée une nouvelle partie
+        Game game = new Game();
+        // Initialise la partie
+        game.setup();
+        // Tant que la partie est en cours
+        while (game.isRunning()) {
+            // Exécute un tour de jeu
+            game.update();
+        }
     }
 }
