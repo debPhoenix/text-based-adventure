f4580f7b9f85eb61154aecb6c4b4fab9a89074bb
Déb <deb.phoenixx@gmail.com>
2021-11-15 18:46:32 +0100

    Add comments in Command interface

--- src/main/java/com/example/entity/command/Command.java
+++ src/main/java/com/example/entity/command/Command.java
@@ -1,6 +1,14 @@
 package com.example.entity.command;
 
+/**
+ * Représente la structure à respecter pour toutes les classes représentant des commandes que le joueur peut entrer
+ */
 public interface Command
 {
+    /**
+     * Tente de traiter une saisie utilisateur
+     * @param userInput La saisie utilisateur à traiter
+     * @return La commande a-t-elle réussi à traiter la saisie utilisateur? (vrai = oui, faux = non)
+     */
     public boolean process(String userInput);
 }
